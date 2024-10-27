e4d820b60599c37c9e1a2c053f25f3351161b46e
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-10-27 23:16:12 +0400

    task_3_Tinatin

--- src/test/java/Task3/Main.java
+++ src/test/java/Task3/Main.java
@@ -1,6 +1,6 @@
 package Task3;
 
-public class MainDog {
+public class Main {
     public static void main(String[] args) {
     Dog dog = new Dog ("shepherd", "meat","Caucasus Shepherd");
     dog.myDog();
